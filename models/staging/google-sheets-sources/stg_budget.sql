b816166de990ee0c60317d297490af1518231570
Daniel Jimenez <flampflamp74@gmail.com>
2023-11-15 18:51:59 +0000

    description add

---
+++ models/staging/google-sheets-sources/stg_budget.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with src_budget as (
+
+    select * from {{ source('google_sheets', 'budget') }}
+
+),
+
+stg_budget as (
+
+    select
+        _row,
+        product_id,
+        quantity,
+        month,
+        _fivetran_synced as date_load
+
+    from src_budget
+
+)
+
+select * from stg_budget
